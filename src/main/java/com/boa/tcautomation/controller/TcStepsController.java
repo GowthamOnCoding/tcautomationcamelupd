9d27b210c753ba3f4e9914e2af778d2c656764e5
gowthamoncoding <vm.gowtham@hotmail.com>
2025-02-11 08:59:15 +0530

    Initial working version commit

--- src/main/java/com/boa/tcautomation/controller/TcStepsController.java
+++ src/main/java/com/boa/tcautomation/controller/TcStepsController.java
@@ -1,7 +1,10 @@
 package com.boa.tcautomation.controller;
 
+import com.boa.tcautomation.model.StepConfig;
+import com.boa.tcautomation.model.TcMaster;
 import com.boa.tcautomation.model.TcSteps;
 import com.boa.tcautomation.service.TcStepsService;
+import com.boa.tcautomation.service.TestAPIService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +17,8 @@ public class TcStepsController {
 
     @Autowired
     private TcStepsService tcStepsService;
+    @Autowired
+    private TestAPIService testManagementService;
 
     @GetMapping("/tcsteps")
     public String listTcSteps(Model model) {
@@ -35,6 +40,16 @@ public class TcStepsController {
     @GetMapping("/tcsteps/add")
     public String addTcSteps(Model model) {
         model.addAttribute("tcSteps", new TcSteps());
+        List<TcMaster> testCases = testManagementService.getAllTestCases();
+        List<StepConfig> stepConfigs = testManagementService.getAllStepConfigs();
+
+        // Add to model
+        model.addAttribute("testCases", testCases);
+        model.addAttribute("stepConfigs", stepConfigs);
+
+        // Fetch and add the step to edit
+        //TcSteps tcStep = testManagementService.getTestCaseStepById(id);
+       // model.addAttribute("tcSteps", tcStep);
         return "tcsteps/edit";
     }
 
